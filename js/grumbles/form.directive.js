caefeef51f8040ffa9b18ec4bf302f89ef11ec44
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-03-29 17:19:53 -0400

    Edit and new

---
+++ js/grumbles/form.directive.js
@@ -0,0 +1,15 @@
+"use strict";
+
+(function(){
+  angular
+  .module("grumbles")
+  .directive("grumbleForm", function(){
+    return{
+      templateUrl: 'js/grumbles/_grumble_form.html',
+      replace: true,
+      scope: {
+        grumble: "="
+      },
+    }
+  });
+})();
